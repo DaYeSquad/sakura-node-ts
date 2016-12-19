d1e492ae52ff2f18615f0f38138258f885b8c972
jiangwei <jiangwei@gagogroup.com>
2016-12-19 23:01:51 +0800

    tslint

--- bin/tslint.sh
+++ bin/tslint.sh
@@ -3,4 +3,4 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$BIN_DIR")"
 
-tslint -c ""${ROOT_DIR}"/tslint.json" ""${ROOT_DIR}"/src/*/*.ts" ""${ROOT_DIR}"/src/*/*/*.ts" ""${ROOT_DIR}"/*.ts"
+tslint -c ""${ROOT_DIR}"/tslint.json" ""${ROOT_DIR}"/src/**/*.ts"
