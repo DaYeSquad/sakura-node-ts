67af7802728037ecba12674d8db89a92a66547dc
huteng <huteng@hutengs-MacBook-Pro.local>
2017-03-10 17:26:03 +0800

    use moment.js to fix to_date bug

--- src/test/api/testvalidator.ts
+++ src/test/api/testvalidator.ts
@@ -35,4 +35,20 @@ describe("Validator", () => {
     chai.expect(validator.hasErrors()).to.equal(true);
     chai.expect(validator.errors[0].reason).to.equal("invalid a");
   });
+
+  it("Test Validator toDate use moment", () => {
+    let a: string = "2016-1-1";
+    let b: string = "2016-01-01";
+    let c: string = `"2016-1-1"`;
+    let d: string = `"2016-01-01"`;
+    let validator: Validator = new Validator();
+    let dateA: Date =  validator.toDate(a);
+    let dateB: Date =  validator.toDate(b);
+    let dateC: Date =  validator.toDate(c);
+    let dateD: Date =  validator.toDate(d);
+    chai.expect(dateA.toISOString()).to.equal("2015-12-31T16:00:00.000Z");
+    chai.expect(dateB.toISOString()).to.equal("2015-12-31T16:00:00.000Z");
+    chai.expect(dateC.toISOString()).to.equal("2015-12-31T16:00:00.000Z");
+    chai.expect(dateD.toISOString()).to.equal("2015-12-31T16:00:00.000Z");
+  });
 });
