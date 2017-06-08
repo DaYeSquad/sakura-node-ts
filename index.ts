41f88c602bd6502383d19265115a7eea51eeecb4
FrankLin <lin.xiaoe.f@gmail.com>
2017-06-08 14:56:06 +0800

    fix(index.ts): export necessary classes

--- index.ts
+++ index.ts
@@ -1,56 +1,69 @@
-// // Copyright 2016 Frank Lin (lin.xiaoe.f@gmail.com). All rights reserved.
-// // Use of this source code is governed a license that can be found in the LICENSE file.
-//
-// // -------------------------------------------------------------------------
-// // Type defines
-// // -------------------------------------------------------------------------
-//
-// export { timestamp, PgQueryResult } from "./src/base/typedefines";
-//
-// // -------------------------------------------------------------------------
-// // Middleware
-// // -------------------------------------------------------------------------
-//
-// export { corsAllowAll } from "./src/middleware/cors";
-// export { haltOnTimedout } from "./src/middleware/timeout";
-//
-// // -------------------------------------------------------------------------
-// // PostgeSQL
-// // -------------------------------------------------------------------------
-//
-// export { SqlQuery } from "./src/sqlquery/sqlquery";
-// export { DeleteQuery } from "./src/sqlquery/deletequery";
-// export { InsertQuery } from "./src/sqlquery/insertquery";
-// export { ReplaceQuery } from "./src/sqlquery/replacequery";
-// export { SelectQuery } from "./src/sqlquery/selectquery";
-// export { UpdateQuery } from "./src/sqlquery/updatequery";
-//
-// export { PgClient } from "./src/database/pgclient";
-// export { PgClientConfig } from "./src/database/pgclientconfig";
-// export { SqlGenerator } from "./src/tools/sqlgenerator";
-//
-// // -------------------------------------------------------------------------
-// // API
-// // -------------------------------------------------------------------------
-//
-// export { ApiError } from "./src/api/apierror";
-// export { HttpResponse, SuccessResponse, BadRequestResponse, NotFoundResponse, RegisterErrorResponse, AuthErrorResponse, ErrorResponse } from "./src/api/httpresponse";
-// export { Validator } from "./src/api/validator";
-//
-// // -------------------------------------------------------------------------
-// // Base
-// // -------------------------------------------------------------------------
-//
-// export { TableName, Column } from "./src/base/decorator";
-// export { Model, SqlFlag, SqlType, SqlField, SqlDefaultValue, SqlDefaultValueType } from "./src/base/model";
-//
-// // -------------------------------------------------------------------------
-// // Utils
-// // -------------------------------------------------------------------------
-// export { sqlContext } from "./src/util/sqlcontext";
-// export { DateFormatter, DateFormtOption } from "./src/util/dateformatter";
-// export { DateUtil, DateEqualityPrecision } from "./src/util/dateutil";
-// export { Migration } from "./src/migration/migration";
-// export { AppMigration } from "./src/migration/appmigration";
-// export { Field } from "./src/migration/column";
-//
+// Copyright 2016 Frank Lin (lin.xiaoe.f@gmail.com). All rights reserved.
+// Use of this source code is governed a license that can be found in the LICENSE file.
+
+// -------------------------------------------------------------------------
+// Type defines
+// -------------------------------------------------------------------------
+
+export { timestamp, PgQueryResult } from "./src/base/typedefines";
+
+// -------------------------------------------------------------------------
+// Middleware
+// -------------------------------------------------------------------------
+
+export { corsAllowAll } from "./src/middleware/cors";
+export { haltOnTimedout } from "./src/middleware/timeout";
+
+// -------------------------------------------------------------------------
+// API
+// -------------------------------------------------------------------------
+
+export { ApiError } from "./src/api/apierror";
+export { HttpResponse, SuccessResponse, BadRequestResponse, NotFoundResponse, RegisterErrorResponse, AuthErrorResponse, ErrorResponse } from "./src/api/httpresponse";
+export { Validator } from "./src/api/validator";
+
+// -------------------------------------------------------------------------
+// Base
+// -------------------------------------------------------------------------
+
+export { TableName, Column } from "./src/base/decorator";
+export { Model, SqlFlag, SqlType, SqlField, SqlDefaultValue, SqlDefaultValueType } from "./src/base/model";
+
+// -------------------------------------------------------------------------
+// Database
+// -------------------------------------------------------------------------
+
+export { SqlQuery } from "./src/sqlquery/sqlquery";
+export { DeleteQuery } from "./src/sqlquery/deletequery";
+export { InsertQuery } from "./src/sqlquery/insertquery";
+export { ReplaceQuery } from "./src/sqlquery/replacequery";
+export { SelectQuery } from "./src/sqlquery/selectquery";
+export { UpdateQuery } from "./src/sqlquery/updatequery";
+
+export * from "./src/database/dbclient";
+export * from "./src/database/driver";
+export * from "./src/database/driveroptions";
+export * from "./src/database/querybuilder";
+export * from "./src/database/queryresult";
+
+export * from "./src/database/error/internalerror";
+export * from "./src/database/error/unknowndrivererror";
+
+export * from "./src/database/migration/column";
+export * from "./src/database/migration/migration";
+export * from "./src/database/migration/operation";
+export * from "./src/database/migration/version";
+
+export * from "./src/database/postgres/pgdriver";
+export * from "./src/database/postgres/pgquerybuilder";
+
+export * from "./src/database/mysql/mysqldriver";
+export * from "./src/database/mysql/mysqlquerybuilder";
+
+// -------------------------------------------------------------------------
+// Utils
+// -------------------------------------------------------------------------
+
+export { sqlContext } from "./src/util/sqlcontext";
+export { DateFormatter, DateFormtOption } from "./src/util/dateformatter";
+export { DateUtil, DateEqualityPrecision } from "./src/util/dateutil";
