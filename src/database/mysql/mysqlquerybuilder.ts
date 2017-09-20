b82270e69fca84e61fd2a470a0c2a362c46d43be
FrankLin <lin.xiaoe.f@gmail.com>
2017-09-20 11:30:34 +0800

    fix(*): comment a test needs docker

--- src/database/mysql/mysqlquerybuilder.ts
+++ src/database/mysql/mysqlquerybuilder.ts
@@ -116,9 +116,8 @@ export class MySqlQueryBuilder implements QueryBuilder {
       let keys: Array<string> = modelSqlInfo.keys;
       let values: Array<string> = modelSqlInfo.values;
 
-
       // 如果主键默认值是随机数， 则在插入时自动添加一个随机数主键
-      let fields: SqlField[] = sqlContext.findSqlFields(q.model_.constructor)
+      let fields: SqlField[] = sqlContext.findSqlFields(q.model_.constructor);
       for (let field of fields) {
         if (field.flag === SqlFlag.PRIMARY_KEY) {
           if (field.defaultValue.type === SqlDefaultValueType.MAKE_RANDOM_ID) {
